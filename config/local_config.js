d0e72b53eed86c48cd78377cc47507f42de78988
Jerum Hubbert <hubbertj@gmail.com>
2017-03-13 22:21:20 -0700

    starting work on the worker

--- config/local_config.js
+++ config/local_config.js
@@ -1,11 +1,3 @@
 module.exports = {
-    environment: 'development',
-    database: {
-        host: 'localhost',
-        port: 5432,
-        user: 'postgres',
-        password: 'postgres',
-        database: 'counter'
-    },
-    log_level: 'local'
+
 };
